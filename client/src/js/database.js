68ff4649e93cc12e29d288782695f750998c69ed
BDA2013 <129307670+BDA2013@users.noreply.github.com>
2023-10-02 14:26:46 -0400

    Added fixed to database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,7 +17,7 @@ export const putDb = async (content) => {
   const db = await openDB('jate', 1);
   const tx = db.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  await store.put(content);
+  await store.put({value: content, id: 1});
   await tx.done;
 }//console.error('putDb not implemented');
 
@@ -26,9 +26,9 @@ export const getDb = async () => {
   const db = await openDB('jate', 1);
   const tx = db.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const result = await store.getAll();
+  const result = await store.get(1);
   await tx.done;
-  return result;
+  return result?.value;
 }//console.error('getDb not implemented');
 
 initdb();
